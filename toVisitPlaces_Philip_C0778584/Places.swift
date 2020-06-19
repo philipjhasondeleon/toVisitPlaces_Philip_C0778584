5ff96bad0b159ee94969d216a61b928c37699a0e
Philip <jhasondeleon@gmail.com>
2020-06-18 21:31:10 -0400

    added bar button to navigate the map

--- toVisitPlaces_Philip_C0778584/Places.swift
+++ toVisitPlaces_Philip_C0778584/Places.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Places{
+    
+    var placeLat: Double
+    var placeLong: Double
+    var placeName :String
+    var city :String
+    var postalCode : String
+    var country : String
+    
+    init(placeLat:Double , placeLong: Double, placeName:String, city:String, postalCode: String, country:String) {
+        self.placeLat = placeLat
+        self.placeLong = placeLong
+        self.placeName = placeName
+        self.city = city
+        self.postalCode = postalCode
+        self.country = country
+    }
+    
+    
+}
--- toVisitPlaces_Philip_C0778584/PlacesTableViewController.swift
+++ toVisitPlaces_Philip_C0778584/PlacesTableViewController.swift
@@ -9,82 +9,129 @@
 import UIKit
 
 class PlacesTableViewController: UITableViewController {
-
+    
+    var places : [Places]?
+    
+    var deleteArray : [Places]?
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+          self.navigationController?.view.tintColor = .systemPink
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadData()
+        self.tableView.reloadData()
+        
+    }
+    
+    func getDataFilePath() -> String {
+           let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+           let filePath = documentPath.appending("/places-data.txt")
+           return filePath
+       }
+    
+    func loadData() {
+        places = [Places]()
+        
+        let filePath = getDataFilePath()
+        
+        if FileManager.default.fileExists(atPath: filePath){
+            do{
+                //creating string of file path
+             let fileContent = try String(contentsOfFile: filePath)
+                
+                let contentArray = fileContent.components(separatedBy: "\n")
+                for content in contentArray{
+                   
+                    let placeContent = content.components(separatedBy: ",")
+                    if placeContent.count == 6 {
+                        let place = Places(placeLat: Double(placeContent[0]) ?? 0.0, placeLong: Double(placeContent[1]) ?? 0.0, placeName: placeContent[2], city: placeContent[3], postalCode: placeContent[4], country: placeContent[5])
+                        places?.append(place)
+                    }
+            }
+            }
+            catch{
+                print(error)
+            }
+        }
+    }
+    
+    func deleteRow() {
+        let filePath = getDataFilePath()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        var saveString = ""
+        for place in self.deleteArray!{
+           saveString = "\(saveString)\(place.placeLat),\(place.placeLong),\(place.placeName),\(place.city),\(place.country),\(place.postalCode)\n"
+            do{
+           try saveString.write(toFile: filePath, atomically: true, encoding: .utf8)
+            }
+            catch{
+                print(error)
+            }
+        }
     }
 
     // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection
+                                section: Int) -> String? {
+       return "Your Favourite Places are:"
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return places?.count ?? 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let place = self.places![indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "placeCell")
+        cell?.textLabel?.text = place.placeName + " , " + place.city
+        cell?.detailTextLabel?.text = place.country + " , " + place.postalCode
 
-        // Configure the cell...
-
-        return cell
+        return cell!
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let editedPlace =  self.places![indexPath.row]
+        
+        defaults.set(editedPlace.placeLat, forKey: "latitude")
+        defaults.set(editedPlace.placeLong, forKey: "longitude")
+        defaults.set(true, forKey: "bool")
+        defaults.set(indexPath.row, forKey: "edit")
+        
+        let newVC = self.storyboard?.instantiateViewController(withIdentifier: "newVC") as! PlaceEditViewController
+        self.navigationController?.pushViewController(newVC, animated: true)
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            self.places?.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            self.deleteArray = self.places
+            deleteRow()
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
